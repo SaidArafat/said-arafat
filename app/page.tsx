bfef6eb77f4851431d5b686d0f78ba6f198c3e54
SaidArafat <s.arafat.selmi@gmail.com>
2024-09-29 22:14:50 +0300

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import type { Metadata } from "next";
+
+export default function Home() {
+  return <section>Initial Commit</section>;
+}
+
+export const metadata: Metadata = {
+  title: "Said Arafat",
+  description: `I'm Said Arafat, a front-end developer with 3 
+    years of experience specializing in React, 
+    TypeScript, and modern web technologies like Tailwind CSS, 
+    Redux-toolkit, React Query and Next.js. 
+    Passionate about building responsive, high-performance applications.`,
+};

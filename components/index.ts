0b2dcf3625ee1b1d10cbf18dd8642c5bd874a2dc
SaidArafat <s.arafat.selmi@gmail.com>
2025-04-02 04:27:34 +0200

    Update components index

--- components/index.ts
+++ components/index.ts
@@ -2,3 +2,5 @@ export { Navbar } from './nav-bar';
 export { Heading } from './headings';
 export { Text } from './text';
 export { Button } from './button';
+export { Card } from './card';
+export { Bar } from './bar';
